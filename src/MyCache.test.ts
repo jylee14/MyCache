beb9fb84f898aae12ec43107b5eef30461df81ff
Jun Lee <jy45lee@gmail.com>
2020-12-03 18:53:00 -0800

    string add/get

--- src/MyCache.test.ts
+++ src/MyCache.test.ts
@@ -4,5 +4,30 @@ describe('MyCache Tests', () => {
     test('Initializing Caches', () => {
         const cache = new MyCache()
         expect(cache).toBeDefined()
+        expect(cache.size).toBe(0)
+    })
+
+    describe('functionalities of the cache', () => {
+        let cache: MyCache
+        beforeEach(() => {
+            cache = new MyCache()
+        })
+        
+        describe('Retrieving from the Cache', () => {
+            test('Empty Cache should return nothing', () => {
+                const nonExistant = cache.get('invalidName')
+                expect(nonExistant.success).toBe(false)
+            })
+
+            test('Cache should return the element that is inserted', () => {
+                const str = "Hello Cache!"
+                
+                cache.add("string", str)
+                expect(cache.size).toBe(1)
+                const fromCache = cache.get("string")
+                expect(fromCache.success).toEqual(true)
+                expect(fromCache.value).toStrictEqual(str)
+            })
+        })
     })
 })
--- src/MyCache.ts
+++ src/MyCache.ts
@@ -1,5 +1,33 @@
-export class MyCache {
-    constructor() {
+export interface CacheResult {
+    success: boolean;
+    value: any | null
+}
 
+export class MyCache {
+    private store: Map<string, any>; // backing store mapping key name -> value 
+    
+    constructor() {
+        this.store = new Map()
+    }
+
+    public get size(): number {
+        return this.store.size
+    }
+
+    get(key: string): CacheResult {
+        if(!(this.store.has(key))) { 
+            return {
+                success: false,
+                value: null
+            }
+        }
+        return { 
+            success: true,
+            value: this.store.get(key)
+        }
+    }
+
+    add(key: string, val: any) {
+        this.store.set(key, val)
     }
 }
